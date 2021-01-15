4c3b12b4cb4b01efca47127157ff2dcf31b7426a
ChristinaAndrinopoyloy <sdi1500006@di.uoa.gr>
2021-01-15 15:14:41 +0200

    second approach almost ready and wordcloud

--- comparing.py
+++ comparing.py
@@ -2,6 +2,14 @@ import helpers as hlp
 import pandas as pd
 
 brainpart_proteins_dict = {'OB':[],'HT':[],'MD':[],'MB':[],'HC':[],'CB':[],'CC':[]}
+brain_parts = ['Cerebellum', 'Cortex', 'Hipocampus', 'Hipothalamus', 'Medulla', 'Mid_Brain', 'Olfactory_balb']
+brain_part_correspond = {'Olfactory_balb':'OB',
+                        'Hipothalamus':'HT',
+                        'Medulla':'MD',
+                        'Mid_Brain':'MB',
+                        'Hipocampus':'HC',
+                        'Cerebellum':'CB',
+                        'Cortex':'CC'}
 
 # read th excel file than contains the proteins from 2DGE
 pathname_2D = './MOUSE BRAIN 2DGE PROTEINS/all.xls'
@@ -17,18 +25,42 @@ for index, row in df_2D.iterrows():
         if key not in brainpart_proteins_dict:
             brainpart_proteins_dict[key] = []
         brainpart_proteins_dict[key].append(protein)
-    for key, value in brainpart_proteins_dict.
         
 
-#     accession_names.append(accesion_name)
-# df_HRMS['Accession Name'] = accession_names
+# for each excel fle from HRMS
+for part in brain_parts:
+    print(part)
+    pathname_HRMS = './MOUSE BRAIN PROTEOME HRMS/'+part+'.xlsx'
+    # common_name = './Results/'+part+'/common_proteins_'+part+'.xls'
+    # unique_name1 = './Results/'+part+'/HRMS_unique_proteins_'+part+'.xls'
+    # unique_name2 = './Results/'+part+'/2DGE_unique_proteins_'+part+'.xls'
 
-# brain_parts = ['Cerebellum', 'Cortex', 'Hipocampus', 'Hipothalamus', 'Medulla', 'Mid_Brain', 'Olfactory_balb', 'all']
+    df_HRMS = hlp.read_from_xlx(pathname_HRMS)
 
-# # for each excel
-# for part in brain_parts:
-#     print(part)
-#     pathname_HRMS = './MOUSE BRAIN PROTEOME HRMS/'+part+'.xlsx'
-#     # common_name = './Results/'+part+'/common_proteins_'+part+'.xls'
-#     # unique_name1 = './Results/'+part+'/HRMS_unique_proteins_'+part+'.xls'
-#     # unique_name2 = './Results/'+part+'/2DGE_unique_proteins_'+part+'.xls'
+    # Add a new column, which contains the Accesion Name for each protein
+    # We extract this information from the description
+    accession_names = []
+    for index, row in df_HRMS.iterrows():
+        description = row['Description']
+        splitted_description = hlp.split_string_based_on_char(description)
+        accesion_name = splitted_description[-1]
+        accesion_name = accesion_name[:-1]
+        accession_names.append(accesion_name)
+    df_HRMS['Accession Name'] = accession_names
+
+
+    # get the proteins (accession name) from the HRMS dataframe for the specific part of the brain
+    proteins_from_HRMS = df_HRMS['Accession Name'].tolist()    
+    # get the proteins (accession name) from the 2DGE dictionary for the specific part of the brain
+    proteins_from_2DGE = brainpart_proteins_dict[brain_part_correspond[part]]
+
+    # print("I am going to compare:")
+    # print(len(proteins_from_HRMS))
+    # print('with')
+    # print(len(proteins_from_2DGE))
+
+    # common proteins
+    common_proteins = list(set(proteins_from_HRMS).intersection(proteins_from_2DGE))
+    print(len(common_proteins))
+    hlp.plot_results(common_proteins, part)
+    print('='*100)
--- helpers.py
+++ helpers.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import numpy as np
+from os import path
+from PIL import Image
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 def read_from_xlx(pathname, lbl_2d=False):
     if lbl_2d:
@@ -10,3 +15,20 @@ def read_from_xlx(pathname, lbl_2d=False):
 def split_string_based_on_char(my_string,separator='['):
     splited_string = my_string.split(sep=separator)
     return splited_string
+
+def plot_results(words, title):
+    mask = np.array(Image.open(path.join("./mouse.png")))
+
+    listToStr = ' '.join([elem for elem in words]) 
+
+    # Create and generate a word cloud image:
+    wordcloud = WordCloud(width=1600, height=800,
+                        background_color="white", max_words=2000, mask=mask,
+                        max_font_size=4, min_font_size=4, colormap="Blues",
+                        contour_width=2, contour_color='steelblue').generate(listToStr)
+
+    # Display the generated image:
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.title(title)
+    plt.show()    
